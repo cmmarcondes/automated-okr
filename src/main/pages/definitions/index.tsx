85264ae759acda458f45e242180b7a11803ae19e
dinocarlos <carlos.santos168@fatec.sp.gov.br>
2021-01-25 15:39:10 -0300

    feat: page definitions e description criada

---
+++ src/main/pages/definitions/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '../../elements/container';
+
+const Goals = () => {
+  return (
+    <Container>
+      <h1>OKR</h1>
+    </Container>
+  );
+}
+
+export default Goals;
